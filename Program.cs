72c66ae7d24d4e5310c571261e4eb78f746c7739
Johnny-27 <johnycal189@gmail.com>
2022-09-29 16:59:58 -0600

    progress

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Random Name generator
+
+Random r = new Random();
+string[] firstname = new string[10]
+{"Bill", "Joe", "Mike", "Allie", "Frank", "Sarah", "Lara", "Jake", "Maddy", "Phil"};
+string[] middlename = new string[10]
+{"Francis", "Carol", "Russ", "Opie", "Ivy", "Cade", "Tony", "Grey", "Lee", "Michael"};
+string[] lastname = new string[10]
+{"Sinatra","Christensen","Smith","Walker","Cobb","Thompson","Adams","Foster","Griffin","Hansen"};
+string[] job = new string[10]
+{"Physicist","Asronaut","Teacher","Spy","Bartender","Cabbie","Test Dummy","Assasin","Construction Worker","Coder",};
+decimal[] hourlywage = new decimal[10]
+{8, 26, 46, 16, 73, 40, 21, 18, 12 ,95};
+decimal[] yearlywage = new decimal[10]
+
+
+
+
+
+
+
+for (int x = 0; x < firstname.Length; x++)
+    {
+        for (int y = 0; x < middlename.Length; y++)
+            {
+                for (int z = 0; z < lastname.Length; z++)
+                    {
+                    
+
+
+
+                    }
+            }
+    }
