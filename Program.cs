6ec19d702626a78d380b3544fc572596d4c5308a
Johnny-27 <johnycal189@gmail.com>
2022-10-06 23:41:43 -0600

    complete generator

--- Program.cs
+++ Program.cs
@@ -7,17 +7,11 @@ string[] middlename = new string[10]
 {"Francis", "Carol", "Russ", "Opie", "Ivy", "Cade", "Tony", "Grey", "Lee", "Michael"};
 string[] lastname = new string[10]
 {"Sinatra","Christensen","Smith","Walker","Cobb","Thompson","Adams","Foster","Griffin","Hansen"};
+string[] fullnames = new string[1000];
 string[] job = new string[10]
 {"Physicist","Asronaut","Teacher","Spy","Bartender","Cabbie","Test Dummy","Assasin","Construction Worker","Coder",};
-decimal[] hourlywage = new decimal[10]
-{8, 26, 46, 16, 73, 40, 21, 18, 12 ,95};
-decimal[] yearlywage = new decimal[10]
-
-
-
-
-
-
+double[] Decimal = new double[]{.25,.5,.75};
+int numberlist = 0;
 
 for (int x = 0; x < firstname.Length; x++)
     {
@@ -25,10 +19,14 @@ for (int x = 0; x < firstname.Length; x++)
             {
                 for (int z = 0; z < lastname.Length; z++)
                     {
-                    
-
-
-
+                    int randomdecimal = r.Next(0,3);
+                    int random = r.Next(0, 5);
+                    int randomwage = r.Next(7,80);
+                    double wage = (double)randomwage;
+                    wage = wage + Decimal[randomdecimal];
+                    fullnames[x] = firstname[x] + middlename[y] + lastname[z] + job[random];
+                    numberlist++;
+                    Console.WriteLine($"{numberlist}: {fullnames[x]}: hourly wage: ${wage}, yearly income: ${wage*48*40}");
                     }
             }
     }
